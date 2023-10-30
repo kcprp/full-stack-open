acc3862555150c56af43395c4ce8364ff961252a
kcprp <kp.szkola@gmail.com>
2023-10-30 21:08:17 +0000

    Exercise 3.20 - Add phone number validation

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -19,7 +19,14 @@ const personSchema = new mongoose.Schema({
     type: String,
     minLength: 3,
     required: true},
-  number: String
+  number: {
+    type: String,
+    validate: {
+        validator: (v) => /^(\d{2,3})-(\d+)$/.test(v),
+        message: props => `${props.value} is not a valid phone number!`
+    },
+    minLength: 8,
+    required:true}
 })
 
 personSchema.set('toJSON', {
