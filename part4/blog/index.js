1d20b72b1e8f3e947ec6aa39926ef0706d9940e3
kcprp <kp.szkola@gmail.com>
2023-11-10 12:44:06 +0000

    Exercise 4.2 - Blog refactor

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,21 +1,6 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blogs')
-require('dotenv').config()
+const app = require('./app')
+const config = require('./utils/config')
 
-Blog = require('./models/blog')
-
-const MONGODB_URI = process.env.MONGODB_URI
-mongoose.connect(MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
