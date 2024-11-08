8c9c395f56caaa48c4275284c35230f05534cbc0
kcprp <kp.szkola@gmail.com>
2024-11-08 12:18:46 +0000

    4.2 - refactor the folder structure

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,36 +1,7 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
 const config = require('./utils/config')
-const Blog = require('./models/blog')
+const logger = require('./utils/logger')
 
-const mongoUrl = config.MONGODB_URI
-console.log(mongoUrl)
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
