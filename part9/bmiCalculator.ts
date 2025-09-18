85b90b2aa54a565b8e7d982278e40acac499fda4
kcprp <kp.szkola@gmail.com>
2025-09-18 11:21:01 +0100

    9.1 - 9.3 - add exercise and bmi functions

---
+++ part9/bmiCalculator.ts
@@ -0,0 +1,14 @@
+const calculateBmi = (height: number, weight: number): string => {
+    const bmi = weight / (height / 100) ** 2;
+    if (bmi < 18.5) {
+        return 'Underweight';
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        return 'Normal weight';
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        return 'Overweight';
+    } else {
+        return 'Obesity';
+    }
+}
+
+console.log(calculateBmi(188, 90));
---
+++ part9/exerciseCalculator.ts
@@ -0,0 +1,44 @@
+interface Result {
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number}
+
+
+const calculateExercies = (exercises: number[], target: number): Result => {
+    if (exercises.length === 0) throw new Error('No exercises provided');
+    if (target <= 0) throw new Error('Target must be positive');
+    if (exercises.some(exercise => exercise < 0)) throw new Error('Exercises must be positive');
+    
+    const average = exercises.reduce((a, b) => a + b, 0) / exercises.length;
+    const periodLength = exercises.length;
+    const trainingDays = exercises.filter(exercise => exercise > 0).length;
+    const success = average >= target;
+    const rating = success ? 3 : average >= target / 2 ? 2 : 1;
+    const ratingDescription = success ? 'You did it! You reached your target.' : average >= target / 2 ? 'Not too bad but could be better' : 'You can do better';
+    return {
+        periodLength,
+        trainingDays,
+        success,
+        rating,
+        ratingDescription,
+        target,
+        average
+    }
+}
+
+try {
+    if (process.argv.length < 4) throw new Error('Not enough arguments');
+    const target = Number(process.argv[2]);
+    const exercises = process.argv.slice(3).map(Number);
+    console.log(calculateExercies(exercises, target));
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.';
+    if (error instanceof Error) {
+        errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+}
